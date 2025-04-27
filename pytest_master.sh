1c9102d3ff30ac4f7817f00bd8d8e1998d02a3d6
garces.eugene2001@gmail.com <garces.eugene2001@gmail.com>
2025-04-27 20:50:13 +0800

    FInal Final

--- pytest_master.sh
+++ pytest_master.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
 pytest Functionality/Compiled Performance/Compiled Security/Compiled Usability/Compiled -v
+
+# Execution of Security Related Tasks that is not dependent to pytest.
+python3 Security/Compiled/BruteForceLogin.py
+
+python3 Security/Compiled/CipherEnum.py
+
+python3 Security/Compiled/SessionManagement.py
